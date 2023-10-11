fd65bd5697db667b90428b37dcc7e3cdb2920718
BuiTriThuc150602 <20088361.thuc@student.iuh.edu.vn>
2023-10-11 14:11:29 +0700

    Init project, navigation

---
+++ ChoeseColor.js
@@ -0,0 +1,104 @@
+import React, {useState} from 'react'
+import { StyleSheet, Text, View, Image, Pressable } from 'react-native'
+
+export default function ChoeseColor({navigation}) {
+    const [colorPicker, setColorPicker] = useState('blue');
+    return (
+        <View>
+            <View style= {styles.decription}>
+                <View><Image
+                source={require('./assets/vs_' + colorPicker + '.png')}
+                style= {styles.imgPhone}  
+                resizeMode='contain'             
+                /></View>
+                <View style = {styles.details}>
+                    <Text>Điện Thoại VSmart Joy 3 - Hàng Chính Hãng</Text>
+                    <Text>Màu : <Text style={styles.curentColorName}>{colorPicker}</Text></Text>
+                    <Text>Cung Cấp Bởi : <Text style={styles.catogeragy}>Tiki Trading</Text></Text>
+                    <Text style={styles.price}>1.790.000đ</Text>                    
+                </View>
+            </View>
+            <View style = {styles.choeseColor}>
+                <Text style={{fontSize:25}}>Chọn một màu bên dưới:</Text>
+                <View style = {styles.color}>
+                    <Pressable style = {[styles.color,styles.sliver]} onPress={()=>{setColorPicker("silver")}}></Pressable>
+                    <Pressable style = {[styles.color,styles.red]} onPress={()=>{setColorPicker("red")}}></Pressable>
+                    <Pressable style = {[styles.color,styles.black]} onPress={()=>{setColorPicker("black")}}></Pressable>
+                    <Pressable style = {[styles.color,styles.blue]} onPress={()=>{setColorPicker("blue")}}></Pressable>
+                </View>
+                <Pressable style = {styles.btnDone} onPress={()=>{navigation.goBack()}}>Xong</Pressable>
+            </View>
+        </View>        
+    )
+}
+
+const styles = StyleSheet.create({
+    decription : {
+        flexDirection : 'row',
+    },
+    imgPhone : {
+        width : 150,
+        height : 180,
+    },
+    details : {
+        marginLeft : 10,
+        paddingVertical : 30,
+        flex : 1,
+        justifyContent : 'space-between',
+    },
+    curentColorName : {
+        fontWeight : 'bold',
+    },
+    catogeragy : {
+        fontWeight : 'bold',
+    },
+    price : {
+        fontWeight : 'bold',
+        fontSize : 20,
+    },
+
+    choeseColor : {
+        padding : 20,
+        flex : 1,
+        backgroundColor : '#C4C4C4',
+    },
+    color : {
+        justifyContent : 'space-between',
+        alignItems : 'center',
+        marginTop : 20,
+    },
+    sliver : {
+        width : 80,
+        height : 80,
+        backgroundColor : '#C5F1FB',
+    },
+    red : {
+        width : 80,
+        height : 80,
+        backgroundColor : '#FF0000',
+    },
+    black : {
+        width : 80,
+        height : 80,
+        backgroundColor : '#000000',
+    },
+    blue : {
+        width : 80,
+        height : 80,
+        backgroundColor : '#0000FF',
+    },
+    btnDone : {
+        backgroundColor : '#1952E294',
+        padding : 10,
+        borderRadius : 10,
+        marginTop : 40,
+        textAlign : 'center',
+        color : '#fff',
+        fontWeight : 'bold',
+        fontSize : 20,
+        borderColor : '#CA1536',
+        borderWidth : 1,
+    },
+
+
+})
---
+++ Home.js
@@ -0,0 +1,134 @@
+import React from "react";
+import { StyleSheet, Text, View, Image, Pressable } from "react-native";
+
+export default function Screen1({navigation, colorPicker}) {
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imgContainer}>
+        <Image
+          style={styles.imgPhone}
+          source={require("./assets/vs_blue.png")}
+        />
+      </View>
+      <View style={styles.decsription}>
+        <Text style={styles.productName}>
+          Điện Thoại VSmart Joy 3 - Hàng Chính Hãng
+        </Text>
+        <View style={styles.ratingContainer}>
+          <View style={styles.rating}>
+            <Image style={styles.star} source={require("./assets/star.png")} />
+            <Image style={styles.star} source={require("./assets/star.png")} />
+            <Image style={styles.star} source={require("./assets/star.png")} />
+            <Image style={styles.star} source={require("./assets/star.png")} />
+            <Image style={styles.star} source={require("./assets/star.png")} />
+          </View>
+          <Text style={styles.ratingText}>Xem thêm (239 đánh giá)</Text>
+        </View>
+        <View style={styles.priceContainer}>
+          <Text style={styles.price}>1.790.000đ</Text>
+          <Text style={styles.discount}>1.790.000đ</Text>
+        </View>
+        <Text style={styles.text}>Ở đâu rẻ hơn hoàn tiền</Text>
+        <Pressable style={styles.selectColor} onPress={()=>navigation.navigate("ChoeseColor")}>Chọn màu sắc <Image style={styles.toLeftVector} source={require("./assets/toLeft.png")}/></Pressable>
+      <Pressable style = {styles.btnBuy}>Mua ngay</Pressable>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width : "100vw",
+    height : "100vh",
+  },
+  imgContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  imgPhone: {
+    width: 300,
+    height: 400,
+    resizeMode: "contain",
+  },
+  decsription: {
+    flex: 1,
+    paddingHorizontal: 20,
+  },
+  productName: {
+    marginBottom : 20,
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  ratingContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom : 20,
+
+  },
+  rating: {
+    flexDirection: "row",
+  },
+  star: {
+    width: 23,
+    height: 25,
+    resizeMode: "contain",
+  },
+  ratingText: {
+    fontStyle: "italic",
+  },
+  priceContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom : 20,
+
+  },
+  price: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  discount: {
+    textDecorationLine: "line-through",
+  },
+  text: {
+    fontWeight: "bold",
+    color: "red",
+    marginBottom : 20,
+
+  },
+  selectColor: {
+    position: "relative",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f0f0f0",
+    textAlign: "center",
+    padding: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    boxShadow: "5px 5px 15px #888888",
+  },
+  toLeftVector : {
+    position : "absolute",
+    right : 10,
+    width : 10,
+    height : 10,
+    resizeMode : "contain",
+    },
+    btnBuy : {
+    backgroundColor : "red",
+    color : "white",
+    textAlign : "center",
+    fontSize: 28,
+    borderRadius : 10,
+    boxShadow : "5px 5px 15px #888888",
+    padding : 8,
+    marginTop : 50,
+       
+    }
+});
